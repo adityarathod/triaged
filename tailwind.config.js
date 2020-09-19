7e294876694d8c52b54e49575e60191b429731f5
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-09-19 16:11:44 -0500

    Add homepage video, logo, and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          `"HK Grotesk", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+        ],
+      },
+    },
   },
   variants: {},
   plugins: [],
