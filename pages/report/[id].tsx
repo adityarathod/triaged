a3999535364f40b8d4ca4b4664f22753f0ecb583
David Richey <darichey1@gmail.com>
2020-09-20 04:55:58 -0500

    styling changes to skin report

--- pages/report/[id].tsx
+++ pages/report/[id].tsx
@@ -47,13 +47,13 @@ const Report = () => {
         <h1 className='text-4xl font-bold'>Skin Report</h1>
         <hr className='my-4' />
 
-        {caseData && feedbackData && (
+        {caseData && (
           <div
             className='w-full flex flex-col shadow-md rounded-lg  px-10 py-4'
             style={{ height: '500px' }}>
             <h2 className='text-3xl font-medium'>Report for {caseData.full_name}</h2>
             <div className='flex flex-row'>
-              <div className='flex-1 p-6'>
+              <div className='p-6'>
                 <p>
                   <span className='font-bold text-xl'>Patient Info</span>
                 </p>
@@ -79,27 +79,32 @@ const Report = () => {
                   <span className='font-medium'>Family history of skin cancer:&nbsp;</span>
                   {caseData.family_related === 'False' ? 'No' : 'Yes'}
                 </p>
+
+                <div className='py-10'>
+                  <p>
+                    <span className='font-bold text-xl'>Provider Feedback</span>
+                  </p>
+                  {feedbackData ? (
+                    <div>
+                      <p>
+                        <span className='font-medium'>Next Steps:&nbsp;</span>
+                        {feedbackData.next_steps}
+                      </p>
+                      <p>
+                        <span className='font-medium'>In person care:&nbsp;</span>
+                        {feedbackData.in_person == 'True' ? 'Yes' : 'No'}
+                      </p>
+                      <p>
+                        <span className='font-medium'>Requires immediate attention:&nbsp;</span>
+                        {feedbackData.next_steps == 'True' ? 'Yes' : 'No'}
+                      </p>
+                    </div>
+                  ) : <p>Awaiting provider feedback. Please check back later.</p>}
+                </div>
               </div>
               <div className='py-6 w-5/12 mx-5'>
                 <img src={caseData.image_link} className='w-full h-auto' />
               </div>
-            </div>
-            <div className='flex-1 p-6'>
-              <p>
-                <span className='font-bold'>Provider Feedback</span>
-              </p>
-              <p>
-                <span className='font-medium'>Next Steps:&nbsp;</span>
-                {feedbackData.next_steps}
-              </p>
-              <p>
-                <span className='font-medium'>In person care:&nbsp;</span>
-                {feedbackData.in_person == 'True' ? 'Yes' : 'No'}
-              </p>
-              <p>
-                <span className='font-medium'>Requires immediate attention:&nbsp;</span>
-                {feedbackData.next_steps == 'True' ? 'Yes' : 'No'}
-              </p>
             </div>
           </div>
         )}
